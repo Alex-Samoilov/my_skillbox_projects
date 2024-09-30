cfba07901dddc44ec5307059548e2db9ac5f1860
Alex.Samoilov <alex.samoilov33@gmail.com>
2024-10-01 01:17:08 +0300

    finish_17.4.2

--- 17.4/2/main.cpp
+++ 17.4/2/main.cpp
@@ -25,43 +25,4 @@ int main () {
 /*
 Напишите функцию, принимающую указатель на тип int, по которому размещены десять переменных типа int. 
 Функция ничего не возвращает, но по тому же указателю элементы должны располагаться в обратном порядке.
-
-
-
-#include <iostream>
-using namespace std;
-
-void reverse(int* arr, int count = 0) {
-    int* arr_2 = arr;
-
-    for (int i = 9; i > 4; i-- ) { 
-        *(arr + count) = *(arr + i);   
-        count++;
-    }
-    for (int i = 4; i > 0 ; i --) {
-        *(arr + count) = *(arr_2 + i);   
-        count++;
-    } 
-}
-
-int main () {
-    int array [10] =  {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int* arr = array;
-    
-    int count = 0;
-    int arr_2 [10] = array;
-
-    for (int i = 9; i > 4; i-- ) { 
-        *(arr + count) = *(arr + i);   
-        count++;
-    }
-    for (int i = 4; i > 0 ; i --) {
-        *(arr + count) = *(arr_2 + i);   
-        count++;
-    } 
-
-    for(int i = 0; i < 10; i++) {
-        cout << *(arr + i) << endl;
-    }
-}
 */
